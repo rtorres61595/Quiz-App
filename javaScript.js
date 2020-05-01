be854f2021a4e881c88526c45644e94b315562f7
Robert Torres <rtorres61595@gmail.com>
2020-05-01 17:41:48 -0400

    added more questions

--- javaScript.js
+++ javaScript.js
@@ -1,18 +1,28 @@
 var questions = [
     {
-        question: "What day is it?",
-        choices: ["mon", "tues", "wed"],
-        answer: "mon"
+        question: "What does it mean to socail distance?",
+        choices: ["High five everyone you see ", "Stay 6 feet apart", "Hide in a corner"],
+        answer: "Stay 6 feet apart"
     },
     {
-        question: "What week is it?",
-        choices: ["mon", "tues", "wed"],
-        answer: "mon"
+        question: "What should you do to stay in shape?",
+        choices: ["Eat all the food in the fridge", "Work out at home", "Cry"],
+        answer: "Work out at home"
     },
     {
-        question: "What mon is it?",
-        choices: ["mon", "tues", "wed"],
-        answer: "mon"
+        question: "Can you leave your home?",
+        choices: ["Only if necessary ", "Wait there's a lock down?", "Any time"],
+        answer: "Only if necessary"
+    },
+    {
+        question: "How much sleep should you get?",
+        choices: ["What is sleep?", "8 hours", "16 hours"],
+        answer: "8 hours"
+    },
+    {
+        question: "What is the best way to spend this extra time we have?",
+        choices: ["Do nothing and stare at a wall", "Plan how you are going to take over the world", "Learn a new skill or hobbie"],
+        answer: "Learn a new skill or hobbie"
     },
     
 ]
@@ -31,12 +41,14 @@ var startBtn = document.getElementById("start-btn");
 startBtn.addEventListener("click", init);
 
 function init() {
+    
     startScreenEl.setAttribute("class", "hide");
     quizScreenEl.removeAttribute("class", "hide");
     displayQuestion();
-}
-
+};
 function displayQuestion() {
+    questions.innerHTML = "";
+
     var currentQuestion = questions[Q];
    
     questionTitleEl.textContent = currentQuestion.question;
@@ -74,8 +86,11 @@ function questionClick() {
     } else {
     displayQuestion();
     }
-}
+};
 
 function endGame() {
     alert("end game");
-}
+    startScreenEl.removeAttribute("class", "hide");
+    
+    
+};
